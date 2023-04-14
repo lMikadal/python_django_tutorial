53c6431d09e03d638792ba3fd6e6b0f8f4e2f630
panupong mikada <path301242@gmail.com>
2023-04-14 22:41:29 +0700

    toturial7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 # import models
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):#StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date indormation", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_field = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
